28334a50cc54e434264f15ba2e7622e1acf017db
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-03-05 22:27:23 -0500

    color

---
+++ multiply.php
@@ -0,0 +1,23 @@
+<?php 
+
+    // $a = 0;
+
+    
+    // if (empty($a)) {
+    //   echo "Variable 'a' is empty.<br>";
+    // }
+    
+   
+    // if (isset($a)) {
+    //   echo "Variable 'a' is set";
+    // }
+
+      if(isset($_POST["a"])) {
+        var 
+        $n = $_POST['n'];
+        $n2 = $_POST['n2'];
+        $add = $n+$n2;
+        $add = "green"
+        echo "Addition Is " ;
+      }
+    ?>
