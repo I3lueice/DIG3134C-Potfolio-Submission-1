e4f881aa3562f043916b147a8df9cf28b29529d4
I3lueice <97864265+I3lueice@users.noreply.github.com>
2022-03-05 22:27:33 -0500

    color

---
+++ color.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+
+if(isset($_POST["a"])) {
+
+  $cl1 = $_POST['cl1'];
+  $cl2 = $_POST['cl2'];
+  $mix = $cl1+$cl2;
+  
+    echo "Your combined color is " .$mix;
+
+}
+
+if($mix = 4) {
+  $mix = "kjhdskahjsbdk";
+}
+
+      // if(isset($_POST["a"])) {
+      //   $n = $_POST['n'];
+      //   $n2 = $_POST['n2'];
+      //   $add = $n+$n2;
+      //   $add = "green"
+      //   echo "Addition Is " ;
+      // }
+    ?>
